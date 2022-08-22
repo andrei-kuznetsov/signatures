e38e3b4b5038d464c635a0420f0e305b4725acbc
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-08-23 01:36:24 +0300

    Make find_signatures twice faster

--- benchmark/signature_benchmark.py
+++ benchmark/signature_benchmark.py
@@ -3,6 +3,7 @@ from benchmark.benchmark_utils import *
 
 logger = logging.getLogger('signature_benchmark')
 
+
 class SignatureBenchmark:
     def __init__(self, benchmark_percent=80, threshold=30, use_rhythmic=False):
         self.benchmark_percent = benchmark_percent
@@ -10,22 +11,11 @@ class SignatureBenchmark:
         self.use_rhythmic = use_rhythmic
 
     def is_similar(self, notes1, notes2):
-        intervals1, directions1, durations1 = parse_ints_from_intervals(notes1)
-        intervals2, directions2, durations2 = parse_ints_from_intervals(notes2)
-        intervals_percent = test_correctness(intervals1, intervals2)
-        direction_percent = test_correctness(directions1, directions2)
+        if len(notes1) != len(notes2):
+            return False  # todo
 
-        if self.use_rhythmic:
-            rhythmic_percent = test_correctness(durations1, durations2)
-        else:
-            rhythmic_percent = 0
-        values = [intervals_percent, direction_percent, rhythmic_percent]
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(str(values) + '\n')
-        return serial_matching(values, self.benchmark_percent) \
-               or parallel_matching(values, self.benchmark_percent) \
-               or summational_matching(values, self.benchmark_percent, self.threshold) \
-               or differential_matching(values, self.benchmark_percent, self.threshold)
+        matching = sum(map(lambda n1, n2: 1 if n1.i == n2.i else 0, notes1, notes2))
+        return matching >= self.benchmark_percent * len(notes1) / 100
 
 
 def parse_ints_from_intervals(notes):
